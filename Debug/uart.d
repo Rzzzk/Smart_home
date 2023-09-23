3e12fd7a70d99afbc93064bf65b22147003f2da4
mahmoud Abdelrahman <123882069+MahmoudAbdelrahman2002@users.noreply.github.com>
2023-09-23 23:30:04 +0300

    test

---
+++ Debug/uart.d
@@ -0,0 +1,9 @@
+uart.o uart.o: ../uart.c ../uart.h \
+  C:\Users\Hp\Desktop\repo_cat\Smart_home\LIB/stdTypes.h \
+  C:\Users\Hp\Desktop\repo_cat\Smart_home\LIB/commonMacros.h
+
+../uart.h:
+
+C:\Users\Hp\Desktop\repo_cat\Smart_home\LIB/stdTypes.h:
+
+C:\Users\Hp\Desktop\repo_cat\Smart_home\LIB/commonMacros.h:
